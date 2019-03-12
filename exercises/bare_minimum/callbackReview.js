1339613948f61d4109e083a864c16e8848dfcb43
Nicholas Miron <nicholasmiron@gmail.com>
2019-03-12 08:06:33 -0500

    Passing tests for callback review

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,24 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, cb) {
   // TODO
+  fs.readFile(filePath, 'utf8', (err, line) => {
+    if (err) {
+      cb(err);
+    } else {
+      cb(null, line.split('\n')[0]);
+    }
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, cb) {
+  if (url.slice(0, 7) !== 'https:/') {
+    cb(new Error('Invalid URI'));
+  } else {
+    cb(null, 200);
+  }
 };
 
 // Export these functions so we can test them and reuse them in later exercises
